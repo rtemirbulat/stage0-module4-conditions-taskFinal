eef2453f0fe1691b1affb379b0437c9bbac8428c
rtemirbulat <equalityisnotequality@gmail.com>
2024-01-31 22:31:44 +0600

    c

--- src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class BitwiseValuesSwap {
     public void swap(int first, int second) {
+        // https://en.wikipedia.org/wiki/XOR_swap_algorithm
+        first = second ^ first;
+        second = first ^ second;
+        first = second ^ first;
+        System.out.println(first);
+        System.out.println(second);
+
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,38 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
-
+        if(year>0 && month>0 && month<=12) {
+            if(month==2){
+                //leap year?
+                if(year%400==0){
+                    System.out.println(29);
+                }
+                else if(year%100==0){
+                    System.out.println(28);
+                }
+                else if(year%4==0){
+                    System.out.println(29);
+                }
+                else System.out.println(28);
+            }
+            else switch (month){
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    System.out.println(30);
+                    break;
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 12:
+                    System.out.println(31);
+                    break;
+            }
+        }
+        else System.out.println("invalid date");
     }
 }
+
